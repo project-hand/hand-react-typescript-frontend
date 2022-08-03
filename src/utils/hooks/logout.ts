10ed3b5b5f3de3808f6ec85c33ef7d3cd71c4809
task11 <task11@naver.com>
2022-08-03 18:08:51 +0900

    feat: logout hooks 구현 & 토큰 제거 테스트 #10

--- src/utils/hooks/logout.ts
+++ src/utils/hooks/logout.ts
@@ -3,16 +3,23 @@ import { useNavigate } from 'react-router-dom';
 import { userState } from '@/store';
 import { logOut } from '@/api';
 
-
 export const useLogout = () => {
   const setUser = useSetRecoilState(userState);
   const navigate = useNavigate();
 
-  async function logout() {
-    await logOut();
-    setUser(null);
-    navigate('/landing');
-  }
+  const logout = async () => {
+    if (!confirm('로그아웃 하시겠습니까?')) {
+      return;
+    }
 
-  return logout;
+    try {
+      await logOut();
+      setUser(null);
+      navigate('/landing');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return { logout };
 };
