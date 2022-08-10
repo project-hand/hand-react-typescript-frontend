05085b49a9229f0f72661efa3c626f43a2e63a9c
task11 <task11@naver.com>
2022-08-10 19:02:48 +0900

    feat: Lazy loading 추가 #13

--- src/router.tsx
+++ src/router.tsx
@@ -6,55 +6,51 @@ import NotFoundPage from '@/pages/NotFound/NotFoundPage';
 import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
 import { useLogin } from '@/utils/hooks';
 import Main from './layouts/Main/Main';
-import HomePage from './pages/Home/HomePage';
-import ProfilePage from './pages/Profile/ProfilePage';
-import SettingPage from './pages/Setting/SettingPage';
-import LandingPage from './pages/Landing/LandingPage';
 
-// const HomePage = lazy(() => import('@/pages/Home/HomePage'));
-// const LandingPage = lazy(() => import('@/pages/Landing/LandingPage'));
-// const ProfilePage = lazy(() => import('@/pages/Profile/ProfilePage'));
-// const SettingPage = lazy(() => import('@/pages/Setting/SettingPage'));
+const HomePage = lazy(() => import('@/pages/Home/HomePage'));
+const LandingPage = lazy(() => import('@/pages/Landing/LandingPage'));
+const ProfilePage = lazy(() => import('@/pages/Profile/ProfilePage'));
+const SettingPage = lazy(() => import('@/pages/Setting/SettingPage'));
 
 const AppRouter = () => {
   const { isLoading } = useLogin();
   if (!isLoading) return <div></div>;
   return (
-    // <Suspense fallback={<LoadingSpinner />}>
-    <Router>
-      <Routes>
-        <Route element={<Main />}>
-          <Route
-            path="/"
-            element={
-              <AuthRouter>
-                <HomePage />
-              </AuthRouter>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <AuthRouter>
-                <ProfilePage />
-              </AuthRouter>
-            }
-          />
-          <Route
-            path="/setting"
-            element={
-              <AuthRouter>
-                <SettingPage />
-              </AuthRouter>
-            }
-          />
-        </Route>
-        <Route path="/landing" element={
-          <LandingPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
-    // </Suspense>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Router>
+        <Routes>
+          <Route element={<Main />}>
+            <Route
+              path="/"
+              element={
+                <AuthRouter>
+                  <HomePage />
+                </AuthRouter>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <AuthRouter>
+                  <ProfilePage />
+                </AuthRouter>
+              }
+            />
+            <Route
+              path="/setting"
+              element={
+                <AuthRouter>
+                  <SettingPage />
+                </AuthRouter>
+              }
+            />
+          </Route>
+          <Route path="/landing" element={
+            <LandingPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 };
 
