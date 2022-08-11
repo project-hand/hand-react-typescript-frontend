3975fbf24fa9cef5f70a99ef9dd1865e83e3d025
task11 <task11@naver.com>
2022-08-11 17:57:31 +0900

    feat: sidebar layout & animation 구현 #16

---
+++ src/layouts/Sidebar/Sidebar.style.tsx
@@ -0,0 +1,48 @@
+import { SidebarStyleProps } from "@/types/sidebarProps";
+import styled from "styled-components";
+
+const StyledSidebar = styled.div<SidebarStyleProps>`
+position: fixed;
+right: 0;
+display: flex;
+height: 500px;
+width: 140px;
+transform: translateX(${props => props.xPosition}px);
+transition: transform 300ms ease-out;
+`;
+
+const StyledSidebarBtnWrap = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const StyledSidebarBtn = styled.button`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 40px;
+font-size: 2rem;
+padding:0;
+margin: 0;
+background-color: #eee;
+border: none;
+border-top-left-radius:10px;
+border-bottom-left-radius: 10px;
+cursor: pointer;
+`;
+
+const StyledSidebarContent = styled.div`
+background-color: #eee;
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 20px;
+gap: 10px;
+width: 100%;
+height: 100%;
+border-top-left-radius: 15px;
+border-bottom-left-radius: 15px;
+`;
+
+export { StyledSidebar, StyledSidebarBtn, StyledSidebarBtnWrap, StyledSidebarContent };
--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,10 +1,9 @@
-import styled from "styled-components";
 import { FaAngleRight } from "@react-icons/all-files/fa/FaAngleRight";
 import { FaAngleLeft } from "@react-icons/all-files/fa/FaAngleLeft";
+import { StyledSidebar, StyledSidebarBtn, StyledSidebarBtnWrap, StyledSidebarContent } from "./Sidebar.style";
+import { SidebarProps } from "@/types/sidebarProps";
 
-const Sidebar = ({ handleSidbar, state, xPosition, children }) => {
-  console.log(xPosition);
-
+const Sidebar = ({ handleSidbar, state, xPosition, children }: SidebarProps) => {
   return (
     <StyledSidebar xPosition={xPosition}>
       <StyledSidebarBtnWrap>
@@ -21,49 +20,5 @@ const Sidebar = ({ handleSidbar, state, xPosition, children }) => {
     </StyledSidebar>
   );
 };
-
-const StyledSidebar = styled.div<{ xPosition: number; }>`
-  position: fixed;
-  right: 0;
-  display: flex;
-  height: 500px;
-  width: 140px;
-  transform: translateX(${props => props.xPosition}px);
-  transition: transform 300ms ease-out;
-`;
-
-const StyledSidebarBtnWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledSidebarBtn = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 40px;
-  font-size: 2rem;
-  padding:0;
-  margin: 0;
-  background-color: #eee;
-  border: none;
-  border-top-left-radius:10px;
-  border-bottom-left-radius: 10px;
-  cursor: pointer;
-`;
-
-const StyledSidebarContent = styled.div`
-  background-color: #eee;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  gap: 10px;
-  width: 100%;
-  height: 100%;
-  border-top-left-radius: 15px;
-  border-bottom-left-radius: 15px;
-`;
 
 export default Sidebar;
