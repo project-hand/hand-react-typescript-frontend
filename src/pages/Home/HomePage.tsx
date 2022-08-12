ec6c75a90f9352052ba5c759feafa396cd15debc
task11 <task11@naver.com>
2022-08-12 18:01:18 +0900

    feat: sidebar 구현  #16

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,9 @@
 import Sidebar from "@/layouts/Sidebar/Sidebar";
 import { userState } from "@/store/user";
 import { useToggleSidebar } from "@/utils/hooks/toggleSidebar";
+import { useState } from "react";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 
@@ -8,7 +11,7 @@ const mock = {
   usingItem: [
     {
       index: 1,
-      name: 'calendar'
+      name: 'calendar',
     },
     {
       index: 2,
@@ -17,10 +20,6 @@ const mock = {
     {
       index: 3,
       name: 'todo'
-    },
-    {
-      index: 4,
-      name: 'weather'
     }
   ],
   sidebarItem: [
@@ -31,26 +30,49 @@ const mock = {
     {
       index: 2,
       name: 'test2'
+    },
+    {
+      index: 3,
+      name: 'weather'
     }
   ]
 };
 
+const SERV_MODE = {
+  MAIN: 'MAIN',
+  CAL: 'CAL',
+  MEMO: 'MEMO',
+  TODO: 'TODO',
+  CUSTOM: 'CUSTOM'
+} as const;
+
+type SERV_MODE = typeof SERV_MODE[keyof typeof SERV_MODE];
+
 const HomePage = () => {
   const user = useRecoilValue(userState);
   const { toggleSidebar, stateSidebar, xPosition } = useToggleSidebar();
+  const [stateMode, setStateMode] = useState(SERV_MODE.MAIN);
 
   return (
     <StyledHome>
-      <StyledItemContainer>
-        {mock.usingItem.map(({ name }) => {
-          return <StyledUsingItem>{name}</StyledUsingItem>;
-        })}
-      </StyledItemContainer>
-      <Sidebar handleSidbar={toggleSidebar} state={stateSidebar} xPosition={xPosition} >
-        {mock.sidebarItem.map(({ name }) => {
-          return <StyledSidebarItem>{name}</StyledSidebarItem>;
-        })}
-      </Sidebar>
+      {stateMode === SERV_MODE.MAIN
+        ?
+        <StyledModeSelectContainer>
+        </StyledModeSelectContainer>
+        :
+        <DndProvider backend={HTML5Backend}>
+          <StyledItemContainer>
+            {mock.usingItem.map(({ name }) => {
+              return <StyledUsingItem>{name}</StyledUsingItem>;
+            })}
+          </StyledItemContainer>
+          <Sidebar handleSidbar={toggleSidebar} state={stateSidebar} xPosition={xPosition} >
+            {mock.sidebarItem.map(({ name }) => {
+              return <StyledSidebarItem>{name}</StyledSidebarItem>;
+            })}
+          </Sidebar>
+        </DndProvider>
+      }
     </StyledHome>
   );
 };
@@ -58,17 +80,32 @@ const HomePage = () => {
 const StyledHome = styled.div`
   width: 100%;
   height: 100%;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledModeSelectContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const StyledItemContainer = styled.div`
   
+  
+  /* display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-rows: repeat(auto-fill, 1fr);
+  gap: 0.5em 1em; */
 `;
 
 const StyledUsingItem = styled.div.attrs({
   draggable: 'true'
 })`
-  width: 300px;
-  height: 300px;
+  width: 250px;
+  height: 250px;
   background-color: #eee;
   border-radius: 15px;
   resize: both;
